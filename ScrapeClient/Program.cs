efa2d32f0ef89c53844129ec5c559cd93467bb2f
栗鑫 <lx389029980@outlook.com>
2018-02-08 22:54:54 +0800

    V1

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using ScrapeLibrary;
+using System.Net.NetworkInformation;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://www.2345.com", @"C:\Users\lx389\Documents\reply.txt");
+            //string value1 = myScrape.ScrapeWebpage("http://www.2345.com"); // 由于ScrapeWebpage方法有两个重载，所以可以以两种方式发挥作用
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScrapeLibrary/Scrape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            WebClient client = new WebClient(); // DownloadString()是一个下载网页的方法，位于System.Net命名空间的WebClient类中
+            return GetWebpage(url);
+        }
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+            File.WriteAllText(@"C:\Users\lx389\Documents\reply.txt", reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string content= client.DownloadString(url);
+            content += "\n\n\nTHAT'S ALL FORKS !";
+            return content;
+        }
+    }
+}
